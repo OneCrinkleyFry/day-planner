a6459b8bdd64cd567e62b470031efaa54a173f32
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-08-28 11:57:57 -0600

    added the time blocks and classes for editing.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
+var hourTasks = JSON.parse(localStorage.getItem("Task")) || [];
+
 var currentDay = moment().format("[Today is] dddd [the] Do [of] MMMM, YYYY");
 $("#currentDay").text(currentDay);
-var checkDay = setInterval(function() {
+
+var checkTime = setInterval(function() {
     if ((moment().format("[Today is] dddd [the] Do [of] MMMM, YYYY")) !== currentDay) {
         currentDay = moment().format("[Today is] dddd [the] Do [of] MMMM, YYYY");
         $("#currentDay").text(currentDay);
     }
 },  1000 * 60 * 10);
+
+$("description").click()
