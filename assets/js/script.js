4130930debfbdd518af0b31f41625be3ddc696c1
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-08-29 13:34:25 -0600

    fixed the color modification check time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,27 @@
 var hourTasks = JSON.parse(localStorage.getItem("tasks")) || ["", "", "", "", "", "", "", "", ""];
 
-var currentDay = moment().format("[Today is] dddd [the] Do [of] MMMM, YYYY");
+var currentDay = moment().format("[Today is] dddd [the] Do [of] MMMM, YYYY ss");
 $("#currentDay").text(currentDay);
 
 var checkTime = setInterval(function() {
     if ((moment().format("[Today is] dddd [the] Do [of] MMMM, YYYY")) !== currentDay) {
-        currentDay = moment().format("[Today is] dddd [the] Do [of] MMMM, YYYY");
+        currentDay = moment().format("[Today is] dddd [the] Do [of] MMMM, YYYY ss");
         $("#currentDay").text(currentDay);
     }
     loadTasks();
-},  1000 * 60 * 10);
+},  1000  * 60 * 10);
 
 
 
 $(".saveBtn").on("click", function () {
     var content = $(this).closest(".time-block").children("textarea").val();
+    var index = $(this).closest(".time-block").attr("id");
+    hourTasks[index] = content;
+    localStorage.setItem("tasks", JSON.stringify(hourTasks));
+});
+
+$(".description").on("blur", function() {
+    var content = $(this).val();
     var index = $(this).closest(".time-block").attr("id");
     hourTasks[index] = content;
     localStorage.setItem("tasks", JSON.stringify(hourTasks));
@@ -30,9 +37,18 @@ var loadTasks = function() {
 };
 
 var checkTime = function() {
-    var currentHour = moment().format("h");
-    $(".container").each(function() {
-        if (currentHour < )
+    var currentHour = moment().format("H");
+    console.log(currentHour);
+    $(".description").each(function() {
+        var hourBlockTime = $(this).attr("data-id");
+        console.log(hourBlockTime);
+        if (hourBlockTime < currentHour) {
+            $(this).removeClass("present future").addClass("past");
+        } else if (currentHour === hourBlockTime) {
+            $(this).removeClass("past future").addClass("present");
+        } else if (currentHour < hourBlockTime) {
+            $(this).removeClass("past present").addClass("future");
+        }
     })
 }
 
